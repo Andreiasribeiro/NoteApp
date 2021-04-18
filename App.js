3f6da69f080799d2ee80b16680f5273d7ffc4eb6
Andréia Ribeiro <asribeirodesign@gmail.com>
2021-04-18 12:44:53 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, StatusBar } from 'react-native';
+import colors from './app/misc/colors';
+import { Intro } from './Intro.js';
+import { NoteScreen } from './NoteScreen.js';
+import {Note} from './Note';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  //find the user to greet
+  const [user, setUser] = useState({});
+  const findUser = async () => {
+
+    const result = await AsyncStorage.getItem('user');
+    if (result !==null) {
+    setUser(JSON.parse(result));
+  }
+  };
+
+  useEffect(() => {
+    //findUser();
+ AsyncStorage.clear();
+  }, []);
+//to appear NoteScreen page if the user is not null (run the app for the first time)
+if (!user.name) return <Intro onFinish = {findUser}></Intro>
+  return <NoteScreen user= {user}></NoteScreen>
 }
 
 const styles = StyleSheet.create({
